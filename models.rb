12f8b9c7e7794355c232dd08c7dba2fcd6ab086a
Stefan <sgk@cs.ucsb.edu>
2008-01-21 07:49:32 -0800

    Redone intro; added paragraph to related work; etc.
    In the process of overhauling the Methodology section.

--- models.rb
+++ models.rb
@@ -14,7 +14,7 @@ fixups = {
 puts <<'HEADER'
 \begin{tabular}{|c|c|p{4.3in}|}
 \multicolumn{1}{c}{\textbf{Level}} &
-\multicolumn{1}{c}{\textbf{Model}} &
+\multicolumn{1}{c}{\textbf{Sub-model}} &
 \multicolumn{1}{c}{\textbf{Overridden and inherited properties}} \\[2pt]
 HEADER
 models.each do |l|
@@ -22,17 +22,17 @@ models.each do |l|
   mods = l[level]
   fixup = fixups[level.to_sym]
   puts '\hline'
-  puts "\\multirow{#{fixup.first}}{*}[#{fixup.last}]{#{level}}"
+  puts "\\multirow{#{fixup.first}}{*}[#{fixup.last}]{\\class{#{level}}}"
   mods.each_with_index do |m,j|
     model = m.keys.first
     items = m[model]
     items = [items] unless items.is_a?(Array)
-    puts "& \\multirow{1}{*}[-0.05em]{#{model}} &"
+    puts "& \\multirow{1}{*}[-0.05em]{\\submodel{#{model}}} &"
     puts '\begin{minipage}[l]{4.3in}'
     puts '\vspace{2pt}'
     items.each_with_index do |item,k|
       item.gsub!(/&/,'\\\&')
-      item.gsub!(/\*\s*$/,'\raisebox{1pt}{\footnotesize*}')
+      item.gsub!(/\*\s*$/,'\raisebox{1.5pt}{\scriptsize*}')
       line_end = (k < items.size-1) ? ' \\\\' : ''
       puts "#{bullet} #{item}" + line_end
     end
